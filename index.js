f57da84b34f0f6b8e6deb337955c3eed56f59a55
Joshua Sigona <sigonasr2@gmail.com>
2020-06-11 10:45:08 +0900

    Implement solution.

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-(function() {
-  'use strict';
-
-  // YOUR CODE HERE
-})();
+document.addEventListener("DOMContentLoaded",()=>{
+	document.getElementById("controls").addEventListener("click",(clicked)=>{
+		var lightPrefix = clicked.target.id.replace("Button","");
+		var targetLight = document.getElementById(lightPrefix+"Light");
+		if (!targetLight.classList.contains(lightPrefix)) {
+			targetLight.classList.add(lightPrefix);
+			console.log(lightPrefix+" bulb on.")
+		} else {
+			targetLight.classList.remove(lightPrefix);
+			console.log(lightPrefix+" bulb off.")
+		}
+	})
+	
+	var buttons = document.getElementsByClassName("button");
+	for (var i=0;i<buttons.length;i++) {
+		buttons[i].addEventListener("mouseenter",(e)=>{
+			console.log("Entered "+e.target.innerHTML+" button.")
+		})
+		buttons[i].addEventListener("mouseleave",(e)=>{
+			console.log("Left "+e.target.innerHTML+" button.")
+		})
+	}
+})
